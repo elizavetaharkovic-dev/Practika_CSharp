347d3f0bfd3e88e3b4dddcb6d6e41f1386e60483
elizavetaharkovic-dev <elizavetaharkovic@gmail.com>
2026-04-29 00:25:20 +0300

    седьмая практика

--- 7/3/InvalidPhoneNumberException.cs
+++
@@ -1,6 +0,0 @@
-﻿class InvalidPhoneNumberException : Exception
-{
-    public InvalidPhoneNumberException() : base() { }
-    public InvalidPhoneNumberException(string message) : base(message) { }
-    public InvalidPhoneNumberException(string message, Exception inner) : base(message, inner) { }
-}
