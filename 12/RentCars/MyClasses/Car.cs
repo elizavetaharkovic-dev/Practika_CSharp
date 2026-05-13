2d146c87f1fea440340a067ab75650a5218f3015
elizavetaharkovic-dev <elizavetaharkovic@gmail.com>
2026-05-13 12:18:10 +0300

    wpf

---
+++ 12/RentCars/MyClasses/Car.cs
@@ -0,0 +1,20 @@
+﻿public class Car
+{
+    public string name { get; set; }
+    public bool status {  get; set; }
+    public double pricePerDay { get; set; }
+    public override string ToString()
+    {
+        string carInfo = "";
+        if (status)
+        {
+            carInfo = $"{name} - Доступно - {pricePerDay} руб/дн";
+            return carInfo;
+        }
+        else
+        {
+            carInfo = $"{name} - Арендовано - {pricePerDay} руб/дн";
+            return carInfo;
+        }
+    }
+}
